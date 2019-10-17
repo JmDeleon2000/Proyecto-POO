2c6d1a7ea8edaa5afb73a3ed03af3c9d9c2f72e7
GuidoPadilla <46847917+GuidoPadilla@users.noreply.github.com>
2019-10-16 21:10:42 -0600

    Update Grafica.java
    Modificacion a grafica de barras

--- src/Grafica.java
+++ src/Grafica.java
@@ -1,55 +1,36 @@
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
-
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.DefaultCategoryDataset;
-
-public class Grafica extends JFrame {
-
-  private static final long serialVersionUID = 1L;
-
-  public Grafica(String title,String descripcion, String titX, String titY, ArrayList<Double> a1) {
-    super(title);
-    // Create dataset
-    DefaultCategoryDataset dataset = createDataset(a1);
-    // Create chart
-    JFreeChart chart = ChartFactory.createLineChart(
-        descripcion, // Chart title
-        titX, // X-Axis Label
-        titY, // Y-Axis Label
-        dataset
-        );
-
-    ChartPanel panel = new ChartPanel(chart);
-    setContentPane(panel);
-  }
-
-  
-  /**
- * @param a1
- * @return el conjunto de datos dictado por los datos de la gradica
- */
-private DefaultCategoryDataset createDataset(ArrayList<Double> a1, String descripcion) {
-
-    String series1 = descripcion;
-
-    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-    
-    int promedio = (a1.size()-1)/20;
-    
-    for(int i=0; i<a1.size();i+= promedio)
-    {
-    	dataset.addValue(a1.get(i), series1, String.valueOf(i));
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.plot.PlotOrientation;
+import java.awt.Dimension;
+import javax.swing.*;
+import java.io.File;
+ 
+public class Archivos extends JFrame {
+ 
+    public Archivos(ArrayList<String> areas, ArrayList<Integer> habitantes){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        DefaultCategoryDataset datasetArchivo = new DefaultCategoryDataset();
+        Integer i = 0;
+        for (i = 0; i<=areas.length-1;i++){
+            dataset.addValue(habitantes.get(i), "Habitantes", areas.get(i));
+        }
+        JFreeChart chart = ChartFactory.createBarChart("Area vs Habitantes",
+                                                        "Areas",
+                                                        "Habitantes",
+                                                        dataset,
+                                                        PlotOrientation.HORIZONTAL, true, true, true);
+        ChartPanel chartPanel = new ChartPanel(chart, false);
+        chartPanel.setPreferredSize(new Dimension(500, 500));
+        setContentPane(chartPanel);
+        try {
+            JFreeChart chartArchivo= ChartFactory.createBarChart("Area vs Habitantes", "Areas", "Habitantes", datasetArchivo, PlotOrientation.HORIZONTAL, true, true, true);
+            ChartUtilities.saveChartAsJPEG(new File("imagen.jpeg"),chartArchivo,this.getHeight(),this.getWidth());
+        }catch (Exception ex) {
+            System.out.println (ex);
+        }
     }
 
-    dataset.addValue(a1.get(a1.size()-1), series1, String.valueOf(a1.size()-1));
-
-
-    return dataset;
-  }
 }
