4cea6c0b5986dc771d6675406a83f0d2aeb04a49
GuidoPadilla <46847917+GuidoPadilla@users.noreply.github.com>
2019-10-16 19:55:57 -0600

    Update Grafica.java
    Desarrollo de codigo en los metodos de grafica

--- src/Grafica.java
+++ src/Grafica.java
@@ -14,7 +14,19 @@ public class Grafica extends JFrame {
   private static final long serialVersionUID = 1L;
 
   public Grafica(String title,String descripcion, String titX, String titY, ArrayList<Double> a1) {
-	  
+    super(title);
+    // Create dataset
+    DefaultCategoryDataset dataset = createDataset(a1);
+    // Create chart
+    JFreeChart chart = ChartFactory.createLineChart(
+        descripcion, // Chart title
+        titX, // X-Axis Label
+        titY, // Y-Axis Label
+        dataset
+        );
+
+    ChartPanel panel = new ChartPanel(chart);
+    setContentPane(panel);
   }
 
   /**
@@ -22,6 +34,19 @@ public class Grafica extends JFrame {
  * @return el conjunto de datos dictado por los datos de la gradica
  */
 private DefaultCategoryDataset createDataset(ArrayList<Double> a1, String descripcion) {
+
+    String series1 = descripcion;
+
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    
+    int promedio = (a1.size()-1)/20;
+    
+    for(int i=0; i<a1.size();i+= promedio)
+    {
+    	dataset.addValue(a1.get(i), series1, String.valueOf(i));
+    }
+
+    dataset.addValue(a1.get(a1.size()-1), series1, String.valueOf(a1.size()-1));
 
 
     return dataset;
