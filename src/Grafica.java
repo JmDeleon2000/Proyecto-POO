fbf57f51c3facf9c8a1ee8d15f5cbb49964d233f
ReVentu <46837148+ReVentu@users.noreply.github.com>
2019-10-16 19:54:07 -0600

    Update Grafica.java

--- src/Grafica.java
+++ src/Grafica.java
@@ -1,18 +1,29 @@
-/**
- * 
- */
+import java.util.ArrayList;
 
-/**
- * @author ASUS
- *
- */
-public class Grafica {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
-	double[] puntos;
-	public Grafica(double[] puntos) 
-	{
-		this.puntos = puntos;
-	}
-	
-	
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class Grafica extends JFrame {
+
+  private static final long serialVersionUID = 1L;
+
+  public Grafica(String title,String descripcion, String titX, String titY, ArrayList<Double> a1) {
+	  
+  }
+
+  /**
+ * @param a1
+ * @return el conjunto de datos dictado por los datos de la gradica
+ */
+private DefaultCategoryDataset createDataset(ArrayList<Double> a1, String descripcion) {
+
+
+    return dataset;
+  }
 }
